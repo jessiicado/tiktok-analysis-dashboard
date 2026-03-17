3f50c3fe199ff1d396f2b0dfc7427b0308fd5735
Jessica Do <jessicanhu.do@gmail.com>
2026-03-17 00:18:18 -0500

    3/16 commit: finished notebooks

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import plotly.graph_objects as go
 import nltk
+import warnings
+warnings.filterwarnings("ignore")
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download("vader_lexicon", quiet=True)
+nltk.download("stopwords")
+nltk.download("punkt")
 
 st.set_page_config(page_title="Tiktok Analytics", layout="wide")
-st.title("TikTok Analytics Dashboard")
-st.write("Setup successful. Ready to Build.")
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/processed/tiktok_sentiment.csv")
+    df["human_time"] = pd.to_datetime(df["human_time"], errors="coerce")
+    return df
+
+df = load_data()
+
+#sidebar
+st.sidebar.title("Filters")
+
+creator_tier = st.sidebar.selectbox(
+    "Creator Tier",
+    ["All", "Low Followers", "High Followers"]
+)
+
+sentiment_filter = st.sidebar.multiselect(
+    "Sentiment",
+    ["positive", "neutral", "negative"],
+    default=["positive", "neutral", "negative"]
+)
+
+duration_range = st.sidebar.slider(
+    "Video duration (seconds)",
+    int(df["duration_seconds"].min()),
+    int(df["duration_seconds"].max()),
+    (int(df["duration_seconds"].min()), int(df["duration_seconds"].max()))      
+)
+
+
